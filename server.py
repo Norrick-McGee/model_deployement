2e82a5f0d407a5ea71410221efd5b31d3b7c9aa8
Norrick <Norrick.mcgee@gmail.com>
2019-05-22 09:07:55 -0500

    integrated NLP model to spam prediction page

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import random
+import model
 
 
 app = Flask(__name__)
@@ -8,7 +9,18 @@ app = Flask(__name__)
 def index():
     return 'Welcome to my page'
 
-@app.route('/dice')
+@app.route('/roll-dice')
 def dice():
     x = random.randint(1,6)
     return f'{x}'
+
+@app.route('/spam-predict')
+def spampred():
+    return render_template('spam_entry.html')
+
+@app.route('/spam-result',methods=['POST'])
+def spam_result():
+    form_input = request.form['spam']
+    #form_input = 'hi'
+    spam = model.predict(form_input)
+    return render_template('spam-result.html', spam=spam)
